13ce5995d884b2b2d756b69f19276d20b56e89c0
Rosana Todosieva <rosanatodosieva100@gmail.com>
2022-09-13 22:43:37 +0200

    commit

--- Web2Tools/urls.py
+++ Web2Tools/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.views.static import serve
@@ -31,8 +30,8 @@ urlpatterns = [
     path('register/', registerPage, name='register'),
     path('exam/', exam, name='exam'),
 
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
