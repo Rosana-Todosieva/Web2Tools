3891b1386591e0a62f5c0ebdb7f4a7c3a43e1640
Rosana Todosieva <rosanatodosieva100@gmail.com>
2022-09-14 23:51:54 +0200

    commit

--- Web2Tools/urls.py
+++ Web2Tools/urls.py
@@ -21,7 +21,7 @@ from tools.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = {
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('addQuestion/', addQuestion, name='addQuestion'),
@@ -30,8 +30,5 @@ urlpatterns = [
     path('register/', registerPage, name='register'),
     path('exam/', exam, name='exam'),
 
-    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-
-]
+}
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
