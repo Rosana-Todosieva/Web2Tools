669d247547d14f81dcb96d01b0006743aea41c56
Rosana Todosieva <rosanatodosieva100@gmail.com>
2022-09-15 00:05:25 +0200

    commit

--- Web2Tools/settings.py
+++ Web2Tools/settings.py
@@ -116,9 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_DIR = BASE_DIR / 'media'
 MEDIA_ROOT = MEDIA_DIR
